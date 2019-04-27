c881a4b5330bd3bb3c39981d418477acf272fbc5
Shubham Dhage <shubhamdhage930@gmail.com>
2019-04-27 17:39:44 +0530

    Updates

---
+++ precache-manifest.a8d0e477f0ba8d07d75de9be4c858a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9e0b58d846368f426a94",
+    "url": "/robofriends-redux/static/js/runtime~main.9e0b58d8.js"
+  },
+  {
+    "revision": "9782b45f744d192273ee",
+    "url": "/robofriends-redux/static/js/main.9782b45f.chunk.js"
+  },
+  {
+    "revision": "ffe0328d6039c64aa3ad",
+    "url": "/robofriends-redux/static/js/1.ffe0328d.chunk.js"
+  },
+  {
+    "revision": "9782b45f744d192273ee",
+    "url": "/robofriends-redux/static/css/main.7fdd0e3b.chunk.css"
+  },
+  {
+    "revision": "ffe0328d6039c64aa3ad",
+    "url": "/robofriends-redux/static/css/1.d3e76ff1.chunk.css"
+  },
+  {
+    "revision": "0df727db9a0cb2829b42ce5d7e3e9a51",
+    "url": "/robofriends-redux/index.html"
+  }
+];
